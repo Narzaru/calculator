421212085e81a035b530b1a24eee0b245c6aee58
Narzaru <yaroslahek@gmail.com>
2022-02-27 19:55:29 +0700

    bgreydon/gui implement test

--- src/main.c
+++ src/main.c
@@ -1,23 +1,43 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-#include "stack/stack.h"
 
-int main(void) {
-    stack_t *stack = create_stack();
+void on_botton_LOG_clicked(GtkButton *b, gpointer user_data);
+void on_botton_LN_clicked(GtkButton *b, gpointer user_data);
+void on_enter(GtkEntry *entry, gpointer  user_data);
 
-    for (int i = 0; i < 512; ++i) {
-        double item = i*1.0;
-        push(&stack, item);
-    }
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv); // init gtk
 
-    while(!is_empty(stack)) {
-        printf("%d ", pop(stack));
-    }
+    GtkTextView *io_field;
+    GtkBuilder *builder;
+    GtkWindow *window;
+    GtkButton *button_ln;
+    GtkButton *button_log;
 
-    pop(stack);
-    pop(stack);
-    pop(stack);
-    pop(stack);
+    builder = gtk_builder_new_from_file("form.glade");
 
-    destroy_stack(&stack);
+    window = GTK_WINDOW(gtk_builder_get_object(builder, "id@gtk_window"));
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_builder_connect_signals(builder, &io_field);
+
+    gtk_window_set_focus_visible(GTK_WINDOW(window), FALSE);
+
+    gtk_widget_show(GTK_WIDGET(window));
+    gtk_main();
+    return 0;
+}
+
+void on_botton_LOG_clicked(GtkButton *b, gpointer io_field) {
+    gtk_widget_set_visible(io_field, FALSE);
+}
+
+void on_botton_LN_clicked(GtkButton *b, gpointer io_field) {
+    gtk_widget_set_visible(io_field, TRUE);
+}
+
+void on_enter(GtkEntry *entry, gpointer  user_data) {
+    GtkEntryBuffer *buffer = gtk_entry_get_buffer(entry);
+    gchar *str = gtk_entry_buffer_get_text(buffer);
+    g_print("%s", str);
 }
